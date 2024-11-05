1892f53fab77f5ed228639b026a711f3875b004f
Ricky8955555 <rkmiao@duck.com>
2024-11-06 00:24:25 +0800

    refactor(markdown): render by calling function

--- static/js/markdown.js
+++ static/js/markdown.js
@@ -1,12 +1,8 @@
 import { marked } from 'https://cdn.jsdelivr.net/npm/marked/+esm';
 import DOMPurify from 'https://cdn.jsdelivr.net/npm/dompurify@3/+esm'
 
-window.addEventListener('load', () => {
-    const elements = document.getElementsByClassName('md-block');
-
-    for (const element of elements) {
-      const content = element.innerText;
-      const html = DOMPurify.sanitize(marked.parse(content));
-      element.innerHTML = html;
-    }
-});
+export const renderMarkdown = element => {
+  const content = element.innerText;
+  const html = DOMPurify.sanitize(marked.parse(content));
+  element.innerHTML = html;
+};

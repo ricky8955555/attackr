7fa4ed1ccf79b228882675a44df580f3842476e8
Ricky8955555 <rkmiao@duck.com>
2024-11-05 18:41:59 +0800

    fix(submission): deprecate submission removal

--- src/pages/admin/submission.rs
+++ src/pages/admin/submission.rs
@@ -4,7 +4,6 @@ use rocket::{
     fairing::AdHoc,
     http::{uri::Origin, CookieJar},
     request::FlashMessage,
-    response::{Flash, Redirect},
 };
 use rocket_dyn_templates::{context, Template};
 
@@ -13,15 +12,14 @@ use crate::{
         query::{
             challenge::list_challenges,
             submission::{
-                delete_challenge_submissions, delete_submission, delete_user_challenge_submissions,
-                delete_user_submissions, list_challenge_submissions, list_submissions,
-                list_user_challenge_submissions, list_user_submissions,
+                list_challenge_submissions, list_submissions, list_user_challenge_submissions,
+                list_user_submissions,
             },
             user::list_users,
         },
         Db,
     },
-    pages::{auth_session, Error, Result, ResultFlashExt},
+    pages::{auth_session, Result},
 };
 
 use super::{check_permission, OptionResponseExt, ResultResponseExt};
@@ -100,57 +98,8 @@ async fn index(
     ))
 }
 
-#[delete("/?<user>&<challenge>")]
-async fn clear(
-    jar: &CookieJar<'_>,
-    db: Db,
-    user: Option<i32>,
-    challenge: Option<i32>,
-) -> Result<Flash<Redirect>> {
-    let current = auth_session(&db, jar).await?;
-    check_permission(&current)?;
-
-    if user.is_none() && challenge.is_none() {
-        return Err(Error::redirect(
-            uri!(ROOT, index(user, challenge)),
-            "禁止删除所有提交记录",
-        ));
-    }
-
-    match (user, challenge) {
-        (None, None) => unreachable!(),
-        (Some(user), None) => delete_user_submissions(&db, user).await,
-        (None, Some(challenge)) => delete_challenge_submissions(&db, challenge).await,
-        (Some(user), Some(challenge)) => {
-            delete_user_challenge_submissions(&db, user, challenge).await
-        }
-    }
-    .flash_expect(uri!(ROOT, index(user, challenge)), "清除提交记录失败")?;
-
-    Ok(Flash::success(
-        Redirect::to(uri!(ROOT, index(user, challenge))),
-        "清除提交记录成功",
-    ))
-}
-
-#[delete("/<id>")]
-async fn delete(jar: &CookieJar<'_>, db: Db, id: i32) -> Result<Flash<Redirect>> {
-    let current = auth_session(&db, jar).await?;
-    check_permission(&current)?;
-
-    delete_submission(&db, id).await.flash_expect(
-        uri!(ROOT, index(None::<i32>, None::<i32>)),
-        "删除提交记录失败",
-    )?;
-
-    Ok(Flash::success(
-        Redirect::to(uri!(ROOT, index(None::<i32>, None::<i32>))),
-        "删除提交记录成功",
-    ))
-}
-
 pub fn stage() -> AdHoc {
-    let routes = routes![index, clear, delete];
+    let routes = routes![index];
 
     AdHoc::on_ignite("Admin Pages - Submission", |rocket| async {
         rocket.mount(ROOT, routes)

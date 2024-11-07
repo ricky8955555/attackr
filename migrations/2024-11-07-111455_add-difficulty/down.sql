2a19e30da4475a9f47d0e38144f5a5094fe76cdc
Ricky8955555 <rkmiao@duck.com>
2024-11-08 00:50:37 +0800

    fix(db): failure on reverting 'add difficulty'

--- migrations/2024-11-07-111455_add-difficulty/down.sql
+++ migrations/2024-11-07-111455_add-difficulty/down.sql
@@ -1,4 +1,25 @@
 -- This file should undo anything in `up.sql`
 
-ALTER TABLE "users" DROP COLUMN "difficulties";
+CREATE TABLE "new_challenges" (
+	"id"	INTEGER,
+	"name"	TEXT NOT NULL,
+	"description"	TEXT NOT NULL,
+	"path"	TEXT NOT NULL,
+	"initial"	REAL NOT NULL,
+	"points"	REAL NOT NULL,
+	"problemset"	INTEGER,
+	"attachments"	TEXT NOT NULL,
+	"flag"	TEXT NOT NULL,
+	"dynamic"	BOOLEAN NOT NULL,
+	"public"	BOOLEAN NOT NULL,
+    "difficulty"	INTEGER,
+	PRIMARY KEY("id"),
+	FOREIGN KEY("problemset") REFERENCES "problemsets"("id") ON DELETE SET NULL
+);
+
+INSERT INTO "new_challenges" SELECT * FROM "challenges";
+DROP TABLE "challenges";
+
+ALTER TABLE "new_challenges" DROP COLUMN "difficulty";
+ALTER TABLE "new_challenges" RENAME TO "challenges";
 DROP TABLE "difficulties";

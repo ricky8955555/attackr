f0bceb036cff05244cd4dbb782a4018d8cb8dc41
Ricky8955555 <rkmiao@duck.com>
2024-11-19 11:33:33 +0800

    style: format code

--- src/functions/user.rs
+++ src/functions/user.rs
@@ -2,11 +2,8 @@ use std::{fs, sync::LazyLock, time::SystemTime};
 
 use anyhow::{anyhow, bail, Result};
 use argon2::{
-    password_hash::{
-        rand_core::OsRng,
-        PasswordHash, PasswordHasher, PasswordVerifier, SaltString
-    },
-    Argon2
+    password_hash::{rand_core::OsRng, PasswordHash, PasswordHasher, PasswordVerifier, SaltString},
+    Argon2,
 };
 use jsonwebtoken::{decode, encode, Algorithm, DecodingKey, EncodingKey, Header, Validation};
 use rocket::{fairing::AdHoc, http::CookieJar, Build, Rocket};
@@ -153,13 +150,17 @@ pub fn hash_password(password: &str) -> Result<String> {
     let salt = SaltString::generate(&mut OsRng);
     let argon2 = Argon2::default();
 
-    Ok(argon2.hash_password(password.as_bytes(), &salt)?.to_string())
+    Ok(argon2
+        .hash_password(password.as_bytes(), &salt)?
+        .to_string())
 }
 
 pub fn verify_password(password: &str, hash: &str) -> Result<bool> {
     let hash = PasswordHash::new(hash)?;
 
-    Ok(Argon2::default().verify_password(password.as_bytes(), &hash).is_ok())
+    Ok(Argon2::default()
+        .verify_password(password.as_bytes(), &hash)
+        .is_ok())
 }
 
 pub fn generate_random() -> String {

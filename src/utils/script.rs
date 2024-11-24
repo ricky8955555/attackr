7f6773238c104935f308a35d9a17b83cbb661a68
Ricky8955555 <rkmiao@duck.com>
2024-11-25 00:55:56 +0800

    feat(script): add exec for script

--- src/utils/script.rs
+++ src/utils/script.rs
@@ -1,5 +1,54 @@
+use std::{io::Read, process::Command};
+
 use anyhow::{anyhow, Result};
-use koto::prelude::*;
+use koto::{prelude::*, runtime::ErrorKind};
+
+fn exec(ctx: &mut CallContext) -> koto::Result<KValue> {
+    match ctx.args() {
+        [KValue::Str(program), KValue::Tuple(targs)] => {
+            let mut args = Vec::with_capacity(targs.len());
+
+            for arg in targs.iter() {
+                match arg {
+                    KValue::Str(val) => args.push(val.as_str()),
+                    unexpected => return type_error("expected str type arg.", unexpected),
+                }
+            }
+
+            let child = Command::new(program.as_str())
+                .args(args)
+                .spawn()
+                .map_err(|err| ErrorKind::StringError(format!("{err:?}")))?;
+
+            let stdout = match child.stdout {
+                Some(mut val) => {
+                    let mut content = String::new();
+
+                    val.read_to_string(&mut content)
+                        .map_err(|err| ErrorKind::StringError(format!("{err:?}")))?;
+
+                    KValue::Str(content.into())
+                }
+                None => KValue::Null,
+            };
+
+            let stderr = match child.stderr {
+                Some(mut val) => {
+                    let mut content = String::new();
+
+                    val.read_to_string(&mut content)
+                        .map_err(|err| ErrorKind::StringError(format!("{err:?}")))?;
+
+                    KValue::Str(content.into())
+                }
+                None => KValue::Null,
+            };
+
+            Ok(KValue::Tuple((&[stdout, stderr]).into()))
+        }
+        unexpected => type_error_with_slice("expected program and args.", unexpected),
+    }
+}
 
 pub struct KotoScript {
     koto: Koto,
@@ -8,6 +57,10 @@ pub struct KotoScript {
 impl KotoScript {
     pub fn compile(script: &str) -> Result<Self> {
         let mut koto = Koto::new();
+
+        let prelude = koto.prelude();
+        prelude.add_fn("exec", exec);
+
         koto.compile_and_run(script)?;
 
         Ok(Self { koto })

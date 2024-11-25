619c311238e5476c6f3dc6b75f9ae977cd39eb0c
Ricky8955555 <rkmiao@duck.com>
2024-11-25 11:37:41 +0800

    feat(build): features

--- build.sh
+++ build.sh
@@ -2,7 +2,14 @@
 
 set -e
 
-cargo build --release
+if [ "$#" -eq 0 ]; then
+    cargo build --release
+elif [ "$#" -eq 1 ]; then
+    cargo build --release -F $1
+else
+    echo "too many arguments."
+    exit 1
+fi
 
 cp target/release/attackr ./
 tar czf attackr.tar.gz attackr static templates

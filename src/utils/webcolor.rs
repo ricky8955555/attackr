ab46e38b72e764fb3765ce641b97d8afc17bd41d
Ricky8955555 <rkmiao@duck.com>
2024-11-28 00:15:39 +0800

    fix(webcolor): validation

--- src/utils/webcolor.rs
+++ src/utils/webcolor.rs
@@ -1,9 +1,13 @@
-use anyhow::{anyhow, Result};
+use anyhow::{anyhow, bail, Result};
 
 pub fn parse_webcolor(expr: &str) -> Result<u32> {
     let hex = expr
         .strip_prefix('#')
         .ok_or_else(|| anyhow!("invalid webcolor format."))?;
+
+    if hex.len() != 6 {
+        bail!("invalid webcolor format.");
+    }
 
     Ok(u32::from_str_radix(hex, 16)?)
 }
